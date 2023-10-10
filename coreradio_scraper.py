d4ea546abf6706828018df755e567e4a96fc1314
antof27 <fncntn00t27c351p@studium.unict.it>
2023-10-10 21:37:32 +0200

    Working on parameters

--- coreradio_scraper.py
+++ coreradio_scraper.py
@@ -28,6 +28,7 @@ def site_requests(attribute, value, page_number):
 
     try:
         response = requests.get(site_url)
+        
         #if the response isn't correctly done, retry after 60 seconds
     except:
         time.sleep(5)
@@ -42,45 +43,59 @@ def site_requests(attribute, value, page_number):
     a_tags = content_div.find_all('a')
 
     for a in a_tags:
-            if "https://coreradio.online/" in a['href'] and "https://coreradio.online/page/" not in a['href'] and a['href'][-1] != "/":
-                
-                id = a['href'].split("/")[4]
-                id = id.split("-")[0]
-                id = int(id)
-                
-                if id is None or id in id_list:
-                    continue
-                else:
-                    id_list.append(id)
+        
+        if "https://coreradio.online/" in a['href'] and "https://coreradio.online/page/" not in a['href'] and a['href'][-1] != "/":
 
-            information = content_div.text.strip()
-
-            lines = information.splitlines()
-            lines = [line for line in lines if line.strip() != ""]
+            id = a['href'].split("/")[4]
+            id = id.split("-")[0]
+            id = int(id)
             
-            subtoken = 0
-            for token in lines:
+            if id is None or id in id_list:
+                continue
+            else:
+                id_list.append(id)
 
-                if token == "more" or token == "MAIN" or token == '«' or token == '»' or token == "Load more" or "Quality:" in token or len(token) <2:
+    information = content_div.text.strip()
+
+    lines = information.splitlines()
+    lines = [line for line in lines if line.strip() != ""]
+    
+    subtoken = 0
+    for token in lines:
+
+        if token == "more" or token == "MAIN" or token == '«' or token == '»' or token == "Load more" or "Quality:" in token or len(token) <2:
+            continue
+        
+        else:
+            if subtoken%3 == 0:
+                try:
+                    genre = token.split(":")[1].strip()
+                    genre = genre.split("/")
+                except:
                     continue
-                
-                else:
-                    if subtoken%3 == 0:
-                        genre = token.split(":")[1].strip()
-                        genre = genre.split("/")
 
-                    elif subtoken%3 == 1:
-                        country = token.split(":")[1].strip()
-                    
-                    elif subtoken%3 == 2:
-                        artist = token.split("-")[0].strip()
-                        song = song_cleaning(token.split("-")[1].strip())
+            elif subtoken%3 == 1:
+                try: 
+                    country = token.split(":")[1].strip()
+                except:
+                    continue
+            
+            elif subtoken%3 == 2:
+                try:
+                    artist = token.split("-")[0].strip()
+                    title = song_cleaning(token.split("-")[1].strip())
 
-                        elements_list = [genre, country, artist, song]
-                        
-                        page_list.append(elements_list)
+                except:
+                    continue
 
-                subtoken = subtoken+1
+        elements_list = [genre, country, artist, title]
+
+        if genre != "" and country != "" and artist != "" and title != "":
+            if elements_list[attribute] == value:
+                page_list.append(elements_list)
+        
+            
+        subtoken = subtoken+1
 
     #time.sleep(5)
 
@@ -88,9 +103,9 @@ def site_requests(attribute, value, page_number):
 
 
 def calling(attribute, value, total_page):
-    for i in range(1, total_page):
-        elements = site_requests(attribute, value, 10)
-        print(elements)
+    for i in range(1, total_page+1):
+        elements = site_requests(attribute, value, i)
+        print("page_list: ", elements)
 
 
-calling(attribute ="country", value="USA", total_page=2)
+calling(attribute ="country", value="USA", total_page=1)
