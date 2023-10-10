311578c6f66b18125f1f6ec1b6d3b1f2b932518d
antof27 <fncntn00t27c351p@studium.unict.it>
2023-10-10 19:24:31 +0200

    Testing site response

--- coreradio_scraper.py
+++ coreradio_scraper.py
@@ -4,11 +4,29 @@ import time
 
 def site_requests(attribute, value, total_pages=20):
     
-    url = "https://coreradio.online/page/"
+    site_url = "https://coreradio.online/page/"
+    
+    id_list = []
+    token_list = []
+    elements_list = []
+
+    genre = ""
+    country= ""
+    artist = ""
+    title = ""
+
     try:
-        response = requests.get(url)
+        response = requests.get(site_url)
         #if the response isn't correctly done, retry after 60 seconds
     except:
         time.sleep(5)
-        response = requests.get(url)
+        response = requests.get(site_url)
 
+    
+    time.sleep(5)
+
+    return response
+
+
+response = site_requests("genre", "rock")
+print(response)
