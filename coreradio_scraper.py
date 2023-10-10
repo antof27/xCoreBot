bbbf3223cb1e715c6d0d38229ec4a0308014245e
antof27 <fncntn00t27c351p@studium.unict.it>
2023-10-10 19:20:10 +0200

    Added libraries and writing the requests function

--- coreradio_scraper.py
+++ coreradio_scraper.py
@@ -1,3 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+import time
+
+def site_requests(attribute, value, total_pages=20):
+    
+    url = "https://coreradio.online/page/"
+    try:
+        response = requests.get(url)
+        #if the response isn't correctly done, retry after 60 seconds
+    except:
+        time.sleep(5)
+        response = requests.get(url)
 
