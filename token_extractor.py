b2b19103e93c9ab4a6320e2bee34421b4b7a94b3
antof27 <fncntn00t27c351p@studium.unict.it>
2023-10-13 12:30:44 +0200

    fix: optional page number case fixed

--- token_extractor.py
+++ token_extractor.py
@@ -8,48 +8,74 @@ def flags_mapping(string):
         'g': 'genre',
         't': 'title'
     }
+    
+    
+    if not all(char in char_to_word for char in string):
+        print("Error: flag not found")
+        return None
 
-    words = [char_to_word[char] for char in string if char in char_to_word]
-
-    return words
+    else:
+        words = [char_to_word[char] for char in string if char in char_to_word]
+        return words
 
 
 def remove_whitespace(string):
     string = string.strip()
     return string
 
+def pages_checker(string, pages=20):
+    if string.split(' ')[-1].isdigit():
+        pages = int(string.split(' ')[-1])
+        string = string.rsplit(' ', 1)[0]
+    return string, pages
+
 
 def arguments_checker(input_string):
     command = input_string.split(" ")[0]
     flags = None
     values = None
+    pages = 20
     if command == "/all":
-        return command, flags, values
+        try:
+            string, pages = pages_checker(input_string, pages)
+            input_string = string
+        except:
+            return command, flags, values, pages
+        
     elif command == "/filter": 
         try:
+            
             flags = input_string.split(" ")[1]
             flags = flags_mapping(flags)
+            if flags == None:
+                return None, None, None, None
+
+            #check if the last element is a number, if so, store it and remove it from the list
+            input_string, pages = pages_checker(input_string, pages)
+            
 
             stringList = input_string.split(' ', 2)
-            tags = stringList[2]            
+            tags = stringList[2]    
+            
             values = tags.split(',')
             values = [remove_whitespace(value) for value in values]
 
             # Check if flags and values have the same length
             if len(flags) != len(values):
                 print("Error: flags and values have different length")
-                return None, None, None 
+                return None, None, None, None
         except:
             print("Error: incorrect syntax")
-            return None, None, None
+            return None, None, None, None
         
 
-    return command, flags, values
+    return command, flags, values, pages
 
 
-    
+
 # Example usage:
-input_string = "/filter -g progressicve"
-command, flags, values = arguments_checker(input_string)
+input_string = "/all 50"
+command, flags, values, pages = arguments_checker(input_string)
+print("Command: ", command, "\nFlags: ", flags, "\nValues: ", values, "\nPages: ", pages)
 
 
