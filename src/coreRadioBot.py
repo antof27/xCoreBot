b1bf56a328d7e8f700e644319c0920ba6e805819
antof27 <fncntn00t27c351p@studium.unict.it>
2024-02-16 18:02:36 +0100

    chore: Pylint Score 9.29/10

--- src/coreRadioBot.py
+++ src/coreRadioBot.py
@@ -1,10 +1,45 @@
-#xCoreBot - module "Bot"
+"""
+xCoreBot - Module for Metal Music Genre Bot
 
+This module contains functionality for a Telegram bot named xCoreBot.
+The bot provides information about the latest releases in the Metal music genre.
+
+Functions:
+- start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  Sends a greeting message to the user when they start interacting with the bot.
+
+- wrong_question(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  Handles unrecognized commands or messages by providing guidance on how to use the bot.
+
+- help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  Provides a list of available commands and their usage instructions.
+
+- print_query_results(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  Retrieves query results based on the user's input command and sends them as messages.
+
+- split_message(text, max_length=4096):
+  Utility function to split a long message into smaller chunks for sending.
+
+- all(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  Handles the '/all' command by extracting arguments, checking their validity,
+  and printing query results.
+
+- filter(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  Handles the '/filter' command by extracting arguments, checking their validity,
+  and printing query results.
+
+- main:
+  Entry point of the module, sets up the bot's handlers and starts the polling loop.
+
+#Run Pylint with the following command: pylint --disable=C0301,E0401,C0103 coreRadioBot.py
+#The import-error can be ignored as it is a false negative error
+"""
+import os
+import sys
 import logging
 from telegram import Update
 from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
-import os
-import sys
+import config
 
 # Get the current script's file path
 script_path = os.path.abspath(__file__)
@@ -16,7 +51,7 @@ parent_directory = os.path.dirname(script_directory)
 sys.path.insert(1, parent_directory)
 from src.coreradio_scraper import query_results
 from src.token_extractor import arguments_checker
-import config
+
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -24,21 +59,52 @@ logging.basicConfig(
 )
 
 
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """
+    Send a greeting message to the user upon starting interaction with the bot.
+
+    Args:
+        update (telegram.Update): The update object containing information about the incoming message.
+        context (telegram.ext.ContextTypes.DEFAULT_TYPE): The context object used for interacting with the Telegram Bot API.
+
+    Returns:
+        None
+    """
     user = update.message.from_user.username
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Hello "+str(user)+", I'm xCoreBot!\nI'm a bot that provides information about the latest releases in the Metal music genre. Use the command '/help' to discover the commands you can use!")
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   text="Hello " + str(user)+", I'm xCoreBot!\nI'm a bot that provides information about the latest releases in the Metal music genre. Use the command '/help' to discover the commands you can use!")
 
 
 async def wrong_question(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """
+    Send a message indicating that the bot couldn't understand the user's input.
+
+    Args:
+        update (telegram.Update): The update object containing information about the incoming message.
+        context (telegram.ext.ContextTypes.DEFAULT_TYPE): The context object used for interacting with the Telegram Bot API.
+
+    Returns:
+        None
+    """
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         text="I didn't understand ...\nuse the command /help to discover which commands you can use!"
-
     )
 
+
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Here is a list of commands you can use:\n\n" \
+    """
+    Provide help information to the user by sending a message with a list of available commands and their usage.
+
+    Args:
+        update (telegram.Update): The update object containing information about the incoming message.
+        context (telegram.ext.ContextTypes.DEFAULT_TYPE): The context object used for interacting with the Telegram Bot API.
+
+    Returns:
+        None
+    """
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   text="Here is a list of commands you can use:\n\n" \
             "/all [n]: returns information about the last n releases, without applying any filter on the releases. " \
             "If n is not specified, the last 20 releases will be returned;\n\n" \
             "/filter -[atgc] [\"artist\", \"title\", \"[genre1+genre2+...+genren], \"country\"] [n]: returns information about the last n releases, " \
@@ -46,8 +112,20 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 
-
 async def print_query_results(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """
+    Process and print the query results to the chat.
+
+    Retrieves the query results based on the input message text, constructs
+    messages for each song with their details, and sends them to the chat.
+
+    Args:
+        update (telegram.Update): The update object containing information about the incoming message.
+        context (telegram.ext.ContextTypes.DEFAULT_TYPE): The context object used for interacting with the Telegram Bot API.
+
+    Returns:
+        None
+    """
     songs = query_results(update.message.text)
     messages = []
 
@@ -56,7 +134,6 @@ async def print_query_results(update: Update, context: ContextTypes.DEFAULT_TYPE
         artist = song[3]
         country = song[1]
         genres = song[0]
-        
         text = f"Title : {title}\nArtist : {artist}\nCountry : {country}\n"
 
         # Iterate genres and append to text
@@ -75,8 +152,18 @@ async def print_query_results(update: Update, context: ContextTypes.DEFAULT_TYPE
     for msg in messages:
         await context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
 
+
 def split_message(text, max_length=4096):
-    """Split a message into chunks."""
+    """
+    Split a message into smaller chunks to ensure it doesn't exceed a maximum length.
+
+    Args:
+        text (str): The message text to be split into chunks.
+        max_length (int, optional): The maximum length of each chunk. Defaults to 4096.
+
+    Returns:
+        list: A list of message chunks, where each chunk has a maximum length of max_length.
+    """
     if len(text) <= max_length:
         return [text]
     chunks = [text[i:i+max_length] for i in range(0, len(text), max_length)]
@@ -87,6 +174,17 @@ def split_message(text, max_length=4096):
 
 
 async def all(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """
+    Handle the '/all' command by retrieving query arguments, checking their validity,
+    and printing query results.
+
+    Args:
+        update (telegram.Update): The update object containing information about the incoming message.
+        context (telegram.ext.ContextTypes.DEFAULT_TYPE): The context object used for interacting with the Telegram Bot API.
+
+    Returns:
+        None
+    """
     # Extracting arguments using tuple unpacking
     c, f, v, n = arguments_checker(update.message.text)
     # Checking if all arguments are None
@@ -95,7 +193,20 @@ async def all(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return
     await print_query_results(update, context)
 
+
+
 async def filter(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """
+    Handle the '/filter' command by retrieving query arguments, checking their validity,
+    and printing query results.
+
+    Args:
+        update (telegram.Update): The update object containing information about the incoming message.
+        context (telegram.ext.ContextTypes.DEFAULT_TYPE): The context object used for interacting with the Telegram Bot API.
+
+    Returns:
+        None
+    """
     # Extracting arguments using tuple unpacking
     c, f, v, n = arguments_checker(update.message.text)
     # Checking if all arguments are None
@@ -103,6 +214,7 @@ async def filter(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await wrong_question(update, context)
         return
     await print_query_results(update, context)
+
 
 
 
@@ -119,5 +231,4 @@ if __name__ == '__main__':
     application.add_handler(help_handler)
     application.add_handler(all_handler)
     application.add_handler(filter_handler)
-
     application.run_polling()
