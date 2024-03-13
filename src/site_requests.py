6a85dab25f3066475f7fc46b04386afe2e2353fe
antof27 <fncntn00t27c351p@studium.unict.it>
2024-03-13 14:39:32 +0100

    chore: added type hints to functions

--- src/site_requests.py
+++ src/site_requests.py
@@ -1,16 +1,17 @@
 """
 Module for making requests and scraping music information from the CoreRadio website.
 """
-#Run Pylint with the following command: pylint --disable=E0401 site_requests.py 
-#The import-error can be ignored as it is a false negative error
+# Run Pylint with the following command: pylint --disable=E0401 site_requests.py 
+# The import-error can be ignored as it is a false negative error
+
 import time
+from typing import List, Tuple
 import requests
 from bs4 import BeautifulSoup
 from src.strings_operations import song_cleaning, values_extractor
 from src.element_processing import process_elements_list
 
-
-def requests_and_soup(url):
+def requests_and_soup(url: str) -> BeautifulSoup:
     """
     Send a request to a URL and return the BeautifulSoup object of the response.
 
@@ -29,7 +30,8 @@ def requests_and_soup(url):
     soup = BeautifulSoup(response.text, "html.parser")
     return soup
 
-def site_requests_maker(command, flags, values, page_number, total_songs, songs_counter):
+def site_requests_maker(command: str, flags: List[str], values: List[str], 
+                        page_number: int, total_songs: int, songs_counter: int) -> Tuple[List[List[str]], int]:
     """
     Scrape the CoreRadio website for music information based on the given command,
     flags, and page number.
@@ -38,24 +40,26 @@ def site_requests_maker(command, flags, values, page_number, total_songs, songs_
         command (str): The command (/all or /filter).
         flags (list): List of flags.
         values (list): List of corresponding values.
+        page_number (int): The page number to scrape.
         total_songs (int): The total number of songs to scrape.
+        songs_counter (int): Counter for the songs.
 
     Returns:
-        list: A list of scraped music information.
+        tuple: A tuple containing a list of scraped music information and the updated songs counter.
     """
     if command == "/filter":
-        query_genre, query_country,\
-              query_artist, query_title = values_extractor(flags, values)
+        query_genre, query_country, query_artist, query_title = values_extractor(flags, values)
     else:
-        query_genre, query_country, \
-            query_artist, query_title = None, None, None, None
+        query_genre, query_country, query_artist, query_title = None, None, None, None
 
     site_url = "https://coreradio.online/page/" + str(page_number)
 
-    page_list = []
-    elements_list = []
-    release_genre = []
-    release_country, release_artist, release_title = None, None, None
+    page_list: List[List[str]] = []
+    elements_list: List[str] = []
+    release_genre: List[str] = []
+    release_country: str = ''
+    release_artist: str = ''
+    release_title: str = ''
     soup = requests_and_soup(site_url)
 
     # Find the content div
@@ -72,8 +76,7 @@ def site_requests_maker(command, flags, values, page_number, total_songs, songs_
 
         if subtoken % 3 == 0:
             try:
-                release_genre = token.split(":")[1].strip()
-                release_genre = release_genre.split("/")
+                release_genre = token.split(":")[1].strip().split("/")
             except IndexError:
                 continue
             subtoken += 1
@@ -96,8 +99,6 @@ def site_requests_maker(command, flags, values, page_number, total_songs, songs_
         if subtoken % 3 == 0:
             elements_list = [release_genre, release_country, release_artist, release_title]
             if songs_counter < total_songs:
-                #print("Elements list: ", elements_list, "songs counter: ",\
-                # songs_counter, "total songs: ", total_songs)
                 songs_counter = process_elements_list(command, elements_list, query_genre,
                                     release_genre, query_country, release_country, query_artist,
                                     release_artist, query_title, release_title, songs_counter,
